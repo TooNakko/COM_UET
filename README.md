112cc3fc69f399e2593a4476a552a3e7e460c87e
N Anh Kiet <anhkietngac@gmail.com>
2023-11-11 14:07:57 +0700

    update files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # COM_UET
+
 Read input from COM and plot the data.
+
+## Note:
+
+This program uses Virtual Serial Port Driver to create and connect virtual COM ports, data writing into COM is generated randomly by `WriteIntoCom.py`. Performance on real situation has not been tested yet.
+
+## How to use:
+
+Install all libraries in `requirements.txt` using `pip install -r requirements.txt`. If something failed to install, download them manually.
+Then run the `run.py`.
+
+## Thanks
---
+++ plot.py
@@ -0,0 +1,53 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import Read_and_Write as RaW
+from datetime import datetime
+
+plt.figure(figsize=(12, 4))
+plt.ion()
+plt.show()
+
+data = np.array([])
+time_stamp = np.array([])
+time_at_start = float(time.time() % (24 * 3600))
+
+txt_log = 'log_data_0.txt'
+folder_name = '_Log_data_texts'
+text_log_path = RaW.GetPathOfText(folder_name,txt_log)
+
+while True:
+    content = RaW.ReadFromSerialCom('COM1',2)
+    #content = serial_com.readline()
+    #content.decode()
+    try:
+        formatted_content = float(content[0:4])
+    except ValueError:
+        print("Done plotting!")
+        break
+    print(formatted_content)
+    current_time = float(time.time() % (24 * 3600)) - time_at_start
+    data = np.append(data, formatted_content)
+    time_stamp = np.append(time_stamp,current_time)
+    plt.cla()
+    plt.xlim(left=0, right=100)
+    plt.ylim(bottom=0, top=10)
+    plt.plot(data[-100:])
+
+    plt.pause(0.015)                                                #Adjust this base on your device's spec so it's sync as much as possible
+plt.close()
+RaW.NewFolder(folder_name)
+
+with open(text_log_path, 'a') as file:
+    file.write("Run at {0}\n".format(time.strftime("%Y-%m-%d %H:%M:%S")))
+    file.write("=========\n")
+    file.write("Time  | Value\n")
+
+    for index in range (0,len(data)):
+        file.write("{0:.3f} | {1:.2f}\n".format(time_stamp[index],data[index]))
+
+print("Text log {0} created!".format(text_log_path))
+
+exit()
+
